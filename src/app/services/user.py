6385c64cf5af5364dc1e090381a0a21557190a9e
negin hajiloo <hajiloo@Net.local>
2026-09-16 16:37:15 +0330

    add user services

---
+++ src/app/services/user.py
@@ -0,0 +1,78 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.models.user import User
+from app.schemas.user import UserCreate, UserUpdate
+
+from app.core.security import hash_password
+
+
+async def create_user(
+    data: UserCreate,
+    db: AsyncSession
+) -> User:
+    
+    user = User(
+        username=data.username, 
+        password=hash_password(data.password),
+        email=data.email
+    )
+
+    db.add(user)
+
+    await db.commit()
+    await db.refresh(user)
+
+    return user
+
+
+async def get_user_by_username(
+    username: str,
+    db: AsyncSession
+) -> User | None:
+    
+    result = await db.execute(
+        select(User)
+        .where(User.username == username)
+    )
+
+    return result.scalar_one_or_none()
+
+
+async def get_user_by_id(
+    user_id: int,
+    db: AsyncSession
+) -> User | None:
+    
+    result = await db.execute(
+        select(User)
+        .where(User.id == user_id)
+    )
+
+    return result.scalar_one_or_none()
+
+
+async def update_user(
+    user: User,
+    data: UserUpdate,
+    db: AsyncSession
+) -> User:
+    
+    updated_data = data.model_dump(exclude_unset=True)
+
+    for field, value in updated_data.items():
+        setattr(user, field, value)
+
+    await db.commit()
+    await db.refresh(user)
+
+    return user
+
+
+async def delete_user(
+    user: User,
+    db: AsyncSession
+) -> None:
+
+    await db.delete(user)
+    await db.commit()
