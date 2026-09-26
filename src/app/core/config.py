1c7a70825d0ac46d3565b934ebac41b0af442e2c
negin hajiloo <hajiloo@Net.local>
2026-09-26 10:16:19 +0330

    improve environment configuration

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -10,9 +10,9 @@ class Settings(BaseSettings):
     test_database_url: str 
 
     jwt_secret_key: str 
-    jwt_algorithm: str = "HS256"
-    access_token_expire_minutes: int = 30
-    refresh_token_expire_days: int = 7
+    jwt_algorithm: str
+    access_token_expire_minutes: int
+    refresh_token_expire_days: int
 
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env",
